82248c4d5d0c4689f23bee18d9a94324bcd80e84
Crowdbotics <team@crowdbotics.com>
2020-12-23 13:29:43 +0000

    remove package blueprint-react-native-blank-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -5,7 +5,6 @@ export const installed_blueprints = [
 { name: 'BlankScreen216126', human_name: 'Blank Screen 2', access_route: 'BlankScreen216126'},
 { name: 'BlankScreen316125', human_name: 'Blank Screen 3', access_route: 'BlankScreen316125'},
 { name: 'BlankScreen016124', human_name: 'Blank Screen 0', access_route: 'BlankScreen016124'},
-{ name: 'BlankScreen016123', human_name: 'Blank Screen 0', access_route: 'BlankScreen016123'},
 { name: 'CopyOfBlankScreen216109', human_name: 'Copy of Blank Screen 2', access_route: 'CopyOfBlankScreen216109'},
 { name: 'CopyOfBlankScreen216106', human_name: 'Copy of Blank Screen 2', access_route: 'CopyOfBlankScreen216106'},
 { name: 'BlankScreen216102', human_name: 'Blank Screen 2', access_route: 'BlankScreen216102'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -10,7 +10,6 @@ import BlankScreen316127Navigator from '../features/BlankScreen316127/navigator'
 import BlankScreen216126Navigator from '../features/BlankScreen216126/navigator';
 import BlankScreen316125Navigator from '../features/BlankScreen316125/navigator';
 import BlankScreen016124Navigator from '../features/BlankScreen016124/navigator';
-import BlankScreen016123Navigator from '../features/BlankScreen016123/navigator';
 import CopyOfBlankScreen216109Navigator from '../features/CopyOfBlankScreen216109/navigator';
 import CopyOfBlankScreen216106Navigator from '../features/CopyOfBlankScreen216106/navigator';
 import BlankScreen216102Navigator from '../features/BlankScreen216102/navigator';
@@ -28,7 +27,6 @@ BlankScreen316127: { screen: BlankScreen316127Navigator },
 BlankScreen216126: { screen: BlankScreen216126Navigator },
 BlankScreen316125: { screen: BlankScreen316125Navigator },
 BlankScreen016124: { screen: BlankScreen016124Navigator },
-BlankScreen016123: { screen: BlankScreen016123Navigator },
 CopyOfBlankScreen216109: { screen: CopyOfBlankScreen216109Navigator },
 CopyOfBlankScreen216106: { screen: CopyOfBlankScreen216106Navigator },
 BlankScreen216102: { screen: BlankScreen216102Navigator },
