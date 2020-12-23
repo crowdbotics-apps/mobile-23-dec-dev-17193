81902706efdf486f2b26cc5c9613270b1d1e0810
Crowdbotics <team@crowdbotics.com>
2020-12-23 08:20:54 +0000

    Update components

--- src/features/CopyOfBlankScreen216108/screens/blank.js
+++ src/features/CopyOfBlankScreen216108/screens/blank.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import {
   View,
   Image,
@@ -10,32 +10,79 @@ import {
   TextInput,
   StyleSheet,
   ScrollView
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
 export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
+  static navigationOptions = ({ navigation }) => {
     return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
-  
-  state = {};
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = {
+    TextInput_3:
+      " jhgjhgjhg jhgjhghj jhgfhjghjg jhgfjhgjhg jhgfjhghjg jhghjghjg jhgjhghjg jhgfhjgjhg jhgjhghfhgfghf hgfhgf jhgjhgjh jhgjhgj jhgjhg jhgjhgjh jhjgjh jhgjhgjh jhgjhgjfh jhghjg jhgjhgjh jhgjhg",
+    TextInput_4: "",
+    TextInput_5:
+      " jhgjhgjhg jhgjhghj jhgfhjghjg jhgfjhgjhg jhgfjhghjg jhghjghjg jhgjhghjg jhgfhjgjhg jhgjhghfhgfghf hgfhgf jhgjhgjh jhgjhgj jhgjhg jhgjhgjh jhjgjh jhgjhgjh jhgjhgjfh jhghjg jhgjhgjh jhgjhg"
+  }
 
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
+    <View>
+      <Text>Sample text content</Text>
+      <TextInput
+        placeholder="Sample text input placeholder"
+        multiline={true}
+        style={styles.TextInput_3}
+        value={this.state.TextInput_3}
+        onChangeText={nextValue => this.setState({ TextInput_3: nextValue })}
+      />
+      <TextInput
+        placeholder="Number Input Placeholder"
+        keyboardType="numeric"
+        value={this.state.TextInput_4}
+        onChangeText={nextValue => this.setState({ TextInput_4: nextValue })}
+      />
+      <TextInput
+        placeholder="Sample text input placeholder"
+        multiline={true}
+        style={styles.TextInput_5}
+        value={this.state.TextInput_5}
+        onChangeText={nextValue => this.setState({ TextInput_5: nextValue })}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 16,
+  View_1: {},
+  Text_2: {},
+  TextInput_3: {
+    height: 100,
+    borderWidth: 3,
+    borderLeftWidth: 3,
+    borderRightWidth: 3,
+    borderTopWidth: 3,
+    borderBottomWidth: 3,
+    textAlign: "center",
+    textTransform: "uppercase",
+    letterSpacing: 1
   },
-});
+  TextInput_4: {},
+  TextInput_5: {
+    height: 100,
+    borderWidth: 3,
+    borderLeftWidth: 3,
+    borderRightWidth: 3,
+    borderTopWidth: 3,
+    borderBottomWidth: 3,
+    textAlign: "center",
+    textTransform: "uppercase",
+    letterSpacing: 1
+  }
+})
