c7f27eb21d6122c45b0db45023ec9164549ecca3
Crowdbotics <team@crowdbotics.com>
2020-12-23 09:15:14 +0000

    Update components

--- src/features/BlankScreen016099/screens/blank.js
+++ src/features/BlankScreen016099/screens/blank.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import {
   View,
   Image,
@@ -10,32 +10,57 @@ import {
   TextInput,
   StyleSheet,
   ScrollView
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
 export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
+  static navigationOptions = ({ navigation }) => {
     return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
-  
-  state = {};
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = {}
 
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
+    <View>
+      <View style={styles.View_2}>
+        <View>
+          <Text>Sample text content</Text>
+          <Text>Sample text content</Text>
+          <Text>Sample text content</Text>
+          <Text>Sample text content</Text>
+          <Text>Sample text content</Text>
+          <Text>Sample text content</Text>
+          <Text>Sample text content</Text>
+          <Text>Sample text content</Text>
+        </View>
+        <View>
+          <Text>Sample text content</Text>
+          <Button title="Press me!" onPress={() => alert("Pressed!")} />
+        </View>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 16,
-  },
-});
+  View_1: {},
+  View_2: { flexDirection: "row" },
+  View_3: {},
+  Text_7: {},
+  Text_17: {},
+  Text_18: {},
+  Text_19: {},
+  Text_20: {},
+  Text_21: {},
+  Text_22: {},
+  Text_23: {},
+  View_4: {},
+  Text_16: {},
+  Button_12: {}
+})
